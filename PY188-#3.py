0d61620c6ef3f6aa008cc62d450ea91bd0ce68c2
十三尾 <505338616@qq.com>
2020-02-01 14:42:18 +0800

    Crossin 教室 Python 入门 第三部分练习-字符串与类型转换

---
+++ PY188-#3.py
@@ -0,0 +1,31 @@
+#第一题
+print('#1')
+name = 'Crossin'
+age = 18
+code = 'Python'
+
+result = ('%s is %d,he writes %s'%(name,age,code))
+print(result)
+#第二题
+print('#2')
+
+num1 = '3.3'
+num2 = 2.5
+
+num1 = float(num1)
+
+
+print(num1+num2)
+
+#第三题
+print('#3')
+
+print(bool(-123))
+print(bool(0))
+print(bool('abc'))
+print(bool('False'))
+print(bool(''))
+print(bool([]))
+print(bool({}))
+print(bool(['']))
+print(bool(None))
