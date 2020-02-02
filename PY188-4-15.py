2b51808119a85057eb101beac598bf0a0bb97eca
十三尾 <505338616@qq.com>
2020-02-03 00:05:27 +0800

    Crossin编程教室 Python入门教程 4-15. 编程实例 - BMI 指数测试

--- PY188-4-15.py
+++ PY188-4-15.py
@@ -1,3 +1,19 @@
+#1. if条件判断
+money = 88
+
+# 当 money 的值大于 100 时，输出 "rich"
+# 否则，输出 "poor"
+
+if money>100:
+    print("rich")
+else:
+    print("poor")
+
+
+
+
+#3. BMI 计算器
+
 print('BMI Calculaion:')
 hight=float(input('plese input your hight(m):'))
 weight=float(input('plese input your weight(kg):'))
